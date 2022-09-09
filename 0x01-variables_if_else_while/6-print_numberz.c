f4a8da1a43009e9965330f6e93d19b1826340ff7
Kelvinklein205 <kelvklein2005@gmail.com>
2022-09-09 15:50:56 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -> assign a variable to the function and print the all
+ * single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
