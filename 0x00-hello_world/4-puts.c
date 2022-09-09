671d3a27d9df9a47ecd9bc01d633796054c6350c
Kelvinklein205 <kelvklein2005@gmail.com>
2022-09-09 02:00:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * init main()
+ * main - Entry point
+ * puts
  *
- * func puts
- * return (0)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
