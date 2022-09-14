463537f6fd12d92bb429470d90aad0ac32616a71
Kelvinklein205 <kelvklein2005@gmail.com>
2022-09-13 17:29:08 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - Prints the first 98 Fibonacci numbers
- * Return: Always 0.
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int c, boolean, boolean2;
-long int n1, n2, fn, fn2, n11, n22;
-n1 = 1;
-n2 = 2;
-boolean =  boolean2 = 1;
-printf("%ld, %ld", n1, n2);
-for (c = 0; c < 96; c++)
-{
-if (boolean)
-{
-fn = n1 + n2;
-printf(", %ld", fn);
-n1 = n2;
-n2 = fn;
-}
-else
-{
-if (boolean2)
-{
-n11 = n1 % 1000000000;
-n22 = n2 % 1000000000;
-n1 = n1 / 1000000000;
-n2 = n2 / 1000000000;
-boolean2 = 0;
-}
-fn2 = (n11 + n22);
-fn = n1 + n2 + (fn2 / 1000000000);
-printf(", %ld", fn);
-printf("%ld", fn2 % 1000000000);
-n1 = n2;
-n11 = n22;
-n2 = fn;
-n22 = (fn2 % 1000000000);
-}
-if (((n1 + n2) < 0) && boolean == 1)
-boolean = 0;
-}
-printf("\n");
-return (0);
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
 }
