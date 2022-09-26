7c99d52badf6756bbdab01a5364136a50516bb16
Kelvinklein205 <kelvklein2005@gmail.com>
2022-09-26 14:01:42 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - length of a prefix substring
+ * @s: string
+ * @accept: substring
+ * Return: numbers of bytes in the init segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int k = 0, c, e = 0;
+
+	while (accept[k])
+	{
+		c = 0;
+
+		while (*(s + c) != 32)
+		{
+			if (*(accept + k) == *(s + c))
+			{
+				e++;
+			}
+			k++;
+		}
+		k++;
+	}
+}
