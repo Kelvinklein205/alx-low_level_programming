7b4c6b3751b35bfc4765c991a364ad262bf470da
Kelvinklein205 <kelvklein2005@gmail.com>
2022-09-26 14:58:23 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - function that locate a substring
+ * @haystack: source string
+ * @needle: substring
+ * Return: NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int b = 0, c;
+
+	while (haystack[b])
+	{
+		while (needle[c])
+		{
+			c = 0;
+		if (haystack[b + c] != needle[c])
+		{
+			break;
+		}
+		c++;
+		}
+	if (needle[b] == '\0')
+	{
+		return (haystack + b);
+	}
+	b++;
+	}
+	return ('\0');
+}
