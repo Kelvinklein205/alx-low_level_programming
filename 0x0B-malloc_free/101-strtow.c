2278c7b2e2c7dcc93119b0d4995d10b4572d5c7e
Kelvinklein205 <kelvklein2005@gmail.com>
2022-10-04 06:02:49 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,69 +1,67 @@
 #include "main.h"
 #include <stdlib.h>
 
-int w_count(char *str, int size);
-int _strlen_recursion(char *s);
-
+/**
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ *
+ * Return: no return
+ */
+void ch_free_grid(char **grid, unsigned int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
+	}
+}
 
 /**
- * **strtow - Splits a string in two words
- * @str: String
- * Return: Pointer to an array of strings
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
  */
 char **strtow(char *str)
 {
-	char *strcp, **strcon;
-	int len = 0, noWords;
+	char **aout;
+	unsigned int c, height, i, j, a1;
 
-	if (str == NULL || str[0] == 0)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	strcp = str;
-	len = _strlen_recursion(strcp);
-	noWords = w_count(str, len);
-
-	if (noWords < 1)
-		return (NULL);
-
-	strcon = malloc(noWords + 1 * sizeof(char *));
-
-	strcon[0] = malloc(sizeof(char) * 1 + 1);
-
-	return (strcon);
-}
-
-/**
- * w_count - Count the total words in a string
- * @str: String
- * @size: length of the string
- * Return: Count of words Integer
- */
-int w_count(char *str, int size)
-{
-	int i, count = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			height++;
+	aout = malloc((height + 1) * sizeof(char *));
+	if (aout == NULL || height == 0)
 	{
-		if (str[i] != ' ')
-		{
-			while (i < size && str[i] != ' ')
-				i++;
-			count++;
-		}
+		free(aout);
+		return (NULL);
 	}
-
-	return (count);
-}
-
-
-/**
- * _strlen_recursion - Length of a string
- * @s: char pointer
- * Return: Integer variable
- */
-int _strlen_recursion(char *s)
-{
-	if (*s != '\0')
-		return (1 + _strlen_recursion(++s));
-	return (0);
+	for (i = a1 = 0; i < height; i++)
+	{
+		for (c = a1; str[c] != '\0'; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					ch_free_grid(aout, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
+	}
+	aout[i] = NULL;
+	return (aout);
 }
