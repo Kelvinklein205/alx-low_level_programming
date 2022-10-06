e51aa2f33f8f06ea79bc6c22938e472545f9e5e7
Kelvinklein205 <kelvklein2005@gmail.com>
2022-10-06 08:44:53 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,10 +7,14 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p = malloc(size);
+	int *p = malloc(nmemb * size);
+	unsigned int n;
 
 	if (nmemb == 0 && size == 0)
 		return (NULL);
 	if (p == NULL)
-		return (NULL);	
+		return (NULL);
+	if (n = 0, n < (nmemb * size), n++)
+		p[n] = 0;
+	return (p);
 }
